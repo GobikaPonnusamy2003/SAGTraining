980f1675b76de865df6a4c71211c8568988ee5cb
GobikaPonnusamy2003 <146160126+GobikaPonnusamy2003@users.noreply.github.com>
2023-10-12 16:04:11 +0530

    Update test.sh

--- task/test.sh
+++ task/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 url="https://itrac.eur.ad.sag/rest/api/latest/issue/WF-30753"
-username="*****"
-password="************"
+username="****"
+password="***************"
 word_to_count="inwardIssue"
 keys_to_extract=".fields.issuelinks[].inwardIssue.key"
 response=$(curl -u "$username:$password" "$url")
@@ -10,6 +10,14 @@ if [ $? -eq 0 ]; then
   extracted_keys=$(echo "$response" | jq --raw-output "$keys_to_extract")
   echo "Word Count for '$word_to_count': $word_count"
   echo "Extracted Keys: $extracted_keys"
+  issuetype_ids=$(echo "$response" | jq --raw-output '.fields.issuelinks[].inwardIssue.fields.issuetype.id')
+  for issuetype_id in $issuetype_ids; do
+    if [ "$issuetype_id" == "15" ]; then
+      issuetype_self_url=$(echo "$response" | jq --raw-output '.fields.issuelinks[] | select(.inwardIssue.fields.issuetype.id == "15") | .inwardIssue.fields.issuetype.self')
+      echo "Self URL for Issuetype ID 15: $issuetype_self_url"
+    fi
+  done
 else
   echo "HTTP request failed with an error"
 fi
+
